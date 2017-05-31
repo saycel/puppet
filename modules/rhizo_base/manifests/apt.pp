eb9c32c7fe74ae3ddfb9a403190c3f299ec83872
jonathan Morales <jonathan.morales26756@gmail.com>
2017-05-31 16:52:28 -0400

    updated icinga repo information to be up to date with ubuntu 14.04.

--- modules/rhizo_base/manifests/apt.pp
+++ modules/rhizo_base/manifests/apt.pp
@@ -15,6 +15,7 @@ class rhizo_base::apt {
   apt::ppa { 'ppa:keithw/mosh': }
   apt::ppa { 'ppa:ondrej/php': }
   apt::ppa { 'ppa:ondrej/apache2': }
+  apt::ppa { 'ppa:formorer/icinga': }
   file { '/etc/apt/apt.conf.d/90unsigned':
       ensure  => present,
       content => 'APT::Get::AllowUnauthenticated "true";',
@@ -26,14 +27,7 @@ class rhizo_base::apt {
       include_src       => false,
       require           => File['/etc/apt/apt.conf.d/90unsigned'],
     }
-  apt::source { 'icinga':
-      location    => 'http://packages.icinga.org/ubuntu',
-      release     => 'icinga-precise',
-      repos       => 'main',
-      key         => 'F51A91A5EE001AA5D77D53C4C6E319C334410682',
-      key_server  => 'pgp.mit.edu',
-      include_src => false,
-    }
+
   apt::source { 'nodesource':
       location    => 'https://deb.nodesource.com/node_0.10',
       release     => 'precise',
